dcea7447f2cc9ee7c515cae66a68194832cc2332
Adlan Hawari <hwr.adlan@gmail.com>
2021-07-13 03:12:38 +0700

    ganti text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mantap wewew
         </p>
         <a
           className="App-link"
